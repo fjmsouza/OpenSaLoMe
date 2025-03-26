c5d20885cae8d2b1c5168c24ec92738aa6dbdf75
Flávio Souza <97055092+fjmsouza@users.noreply.github.com>
2025-03-26 17:37:27 -0300

    corrigindo estrutura do yourcredentials

--- SmartSensorPltfmIO/lib/Credentials/YourCredentials.h
+++ SmartSensorPltfmIO/lib/Credentials/YourCredentials.h
@@ -1,16 +1,8 @@
-#pragma once
 
-class YourCredentialsHandler
-{
-public:
+const char *WIFI_SSID = ""; // Put here your Wi-Fi SSID
+const char *WIFI_PASS = ""; // Put here your Wi-Fi password
 
-    const char *WIFI_SSID = ""; // Put here your Wi-Fi SSID
-    const char *WIFI_PASS = ""; // Put here your Wi-Fi password
-
-    // Google Sheets script ID
-    String GOOGLE_SHEETS_SCRIPT_ID = ""; 
-    // Google Drive Script ID
-    String GOOGLE_DRIVE_SCRIPT_ID = "";
-};
-
-extern YourCredentialsHandler YourCredentials;
+// Google Sheets script ID
+String GOOGLE_SHEETS_SCRIPT_ID = "";
+// Google Drive Script ID
+String GOOGLE_DRIVE_SCRIPT_ID = "";
